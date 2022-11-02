9a913e30e50322dbfa03faf84dd82cb5b812f818
mikkkee-bh <109154394+mikkkee-bh@users.noreply.github.com>
2022-11-02 19:44:40 +0530

    Add files via upload

---
+++ adDomain3.ps1
@@ -0,0 +1,38 @@
+param (
+
+    [parameter(Mandatory = $true)]
+    [string]
+    $domainPassword
+    
+)
+
+$domainName = "ent.bhicorp.com"
+
+$ouPath = 'OU="Managed Servers - Azure",DC=ent,DC=bhicorp,DC=com'
+
+$domainUsername = "BHI-MASTER\svc-BHazVMjoin"
+
+Write-Host "##OUPATH VALUE IS: $ouPath"
+Write-Host "##Domain Name value IS: $domainName"
+Write-Host "##DomainUser Name IS: $domainUsername"
+
+$domainPwd = $domainPassword | ConvertTo-SecureString -AsPlainText -Force
+
+Write-Host "### Domain values--New-Object System.Management.Automation.PSCredential ($domainUsername, $domainPwd)"
+$credential = New-Object pscredential ($domainUsername, $domainPwd)
+
+Write-Host " #### -Credential : '$credential'"
+## Add computer to Domain
+Add-Computer -DomainName $domainName -OUPath $ouPath -Credential $credential
+
+Write-Host "End of Domainjoin and local admin join"
+
+## Add local admin to group
+Add-LocalGroupMember -Group "Administrators" -Member "BHI-MASTER\BHCAzure_Officina2_Windows_HPA"
+
+Write-Host "Waiting to restart with time"
+
+Start-Sleep -Seconds 10
+Write-Host "Restarting the system"
+
+Restart-Computer
